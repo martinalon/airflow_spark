cd92d35e69293a8f55112bc413626f257c06cb29
martinalon <martinalonsofloresgonzalez@gmail.com>
2022-07-30 18:53:27 -0500

    ok

--- dags/spark_emr.py
+++ dags/spark_emr.py
@@ -21,6 +21,14 @@ from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.utils.dates import days_ago
 from airflow.utils.trigger_rule import TriggerRule
 
+DEFAULT_ARGS = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "email": ["airflow@example.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+}
+
 SPARK_STEPS = [
     {
         "Name": "calculate_pi",
@@ -35,66 +43,58 @@ SPARK_STEPS = [
 JOB_FLOW_OVERRIDES = {
     "Name": "PiCalc",
     "ReleaseLabel": "emr-5.29.0",
-    "Applications": [{"Name": "Spark"}],
     "Instances": {
         "InstanceGroups": [
             {
-                "Name": "Primary node",
-                "Market": "ON_DEMAND",
+                "Name": "Master node",
+                "Market": "SPOT",
                 "InstanceRole": "MASTER",
-                "InstanceType": "m5.xlarge",
+                "InstanceType": "m1.medium",
                 "InstanceCount": 1,
-            },
+            }
         ],
-        "KeepJobFlowAliveWhenNoSteps": False,
+        "KeepJobFlowAliveWhenNoSteps": True,
         "TerminationProtected": False,
     },
-    "Steps": SPARK_STEPS,
     "JobFlowRole": "EMR_EC2_DefaultRole",
     "ServiceRole": "EMR_DefaultRole",
 }
 
+with DAG(
+    dag_id="emr_job_flow_manual_steps_dag",
+    default_args=DEFAULT_ARGS,
+    dagrun_timeout=timedelta(hours=2),
+    start_date=days_ago(2),
+    schedule_interval="0 3 * * *",
+    tags=["example"],
+) as dag:
 
-with DAG("db_ingestion", start_date=days_ago(1)) as dag:
-    start_workflow = DummyOperator(task_id="start_workflow")
     # [START howto_operator_emr_manual_steps_tasks]
-    # cluster_creator = EmrCreateJobFlowOperator(
-    #    task_id="create_job_flow",
-    #    job_flow_overrides=JOB_FLOW_OVERRIDES,
-    #    aws_conn_id="aws_default",
-    #    emr_conn_id="emr_default",
-    #    region_name="us-east-2",
-    # )
-    job_flow_creator = EmrCreateJobFlowOperator(
+    cluster_creator = EmrCreateJobFlowOperator(
         task_id="create_job_flow",
         job_flow_overrides=JOB_FLOW_OVERRIDES,
         aws_conn_id="aws_default",
         emr_conn_id="emr_default",
-        region_name="us-east-2",
     )
+
     step_adder = EmrAddStepsOperator(
         task_id="add_steps",
-        job_flow_id=job_flow_creator.output,
+        job_flow_id="{{ task_instance.xcom_pull(task_ids='create_job_flow', key='return_value') }}",
+        aws_conn_id="aws_default",
         steps=SPARK_STEPS,
     )
+
     step_checker = EmrStepSensor(
         task_id="watch_step",
-        job_flow_id=job_flow_creator.output,
+        job_flow_id="{{ task_instance.xcom_pull('create_job_flow', key='return_value') }}",
         step_id="{{ task_instance.xcom_pull(task_ids='add_steps', key='return_value')[0] }}",
+        aws_conn_id="aws_default",
     )
 
     cluster_remover = EmrTerminateJobFlowOperator(
         task_id="remove_cluster",
-        job_flow_id=job_flow_creator.output,
+        job_flow_id="{{ task_instance.xcom_pull(task_ids='create_job_flow', key='return_value') }}",
         aws_conn_id="aws_default",
     )
-    end_workflow = DummyOperator(task_id="end_workflow")
 
-(
-    start_workflow
-    >> job_flow_creator
-    >> step_adder
-    >> step_checker
-    >> cluster_remover
-    >> end_workflow
-)
+    cluster_creator >> step_adder >> step_checker >> cluster_remover
